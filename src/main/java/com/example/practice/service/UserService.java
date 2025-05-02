2f800eed13b1e1ec1cdfd2da43c8fb5891867879
aartichile97 <aartichile9799@gmail.com>
2025-05-02 18:30:43 +0530

    Updated

--- src/main/java/com/example/practice/service/UserService.java
+++ src/main/java/com/example/practice/service/UserService.java
@@ -7,23 +7,23 @@ import org.springframework.stereotype.Service;
 import com.example.practice.config.JwtUtil;
 import com.example.practice.entity.Users;
 import com.example.practice.repository.UserRepository;
+
 @Service
 public class UserService {
 	@Autowired
-    private UserRepository userRepository;
-	
+	private UserRepository userRepository;
 
-    @Autowired
-    private JwtUtil jwtUtil;
+	@Autowired
+	private JwtUtil jwtUtil;
 
-    public String registerUser(Users users) {
-        if (userRepository.findByUsername(users.getUsername()).isPresent()) {
-            return "Username already exists";
-        }
-        userRepository.save(users);
-        return "User registered successfully";
-    }
-    
+	public String registerUser(Users users) {
+		if (userRepository.findByUsername(users.getUsername()).isPresent()) {
+			return "Username already exists";
+		}
+		userRepository.save(users);
+		return "User registered successfully";
+	}
+
 //    public String loginUser(Users users) {
 //        Optional<Users> existingUser = userRepository.findByUsername(users.getUsername());
 //
@@ -37,12 +37,12 @@ public class UserService {
 //            return "User not found";
 //        }
 //    }
-    public String loginUser(Users user) {
-        Optional<Users> existingUser = userRepository.findByUsername(user.getUsername());
-        if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
-            return jwtUtil.generateToken(user.getUsername());
-        }
-        return "Invalid username or password";
-    }
+	public String loginUser(Users user) {
+		Optional<Users> existingUser = userRepository.findByUsername(user.getUsername());
+		if (existingUser.isPresent() && existingUser.get().getPassword().equals(user.getPassword())) {
+			return jwtUtil.generateToken(user.getUsername());
+		}
+		return "Invalid username or password";
+	}
 
 }
